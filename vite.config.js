e7b8a747ab8f75406e81dcdfc7c39f3c6b0a381b
CarpTree <16690855026@163.com>
2026-01-21 11:22:15 +0800

    封装axios

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-// elementPlus按需导入
+// 引入插件
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
+import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,10 +23,22 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    ElementPlus({
+      useSource: true,
+    }),
   ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      },
     },
   },
 });

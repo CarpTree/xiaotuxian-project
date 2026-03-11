5e3f96c031015fb3ffadc7a83ad7ac347bb39a64
CarpTree <16690855026@163.com>
2026-03-11 21:01:58 +0800

    请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useUserStore } from "@/stores/user";
 
 const http = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -8,6 +9,11 @@ const http = axios.create({
 // 请求拦截器
 http.interceptors.request.use(
   (config) => {
+    const userStore = useUserStore();
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (e) => Promise.reject(e),

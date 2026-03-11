e54b81c6f08b6d5e94edf38a4acbbec253a7908b
CarpTree <16690855026@163.com>
2026-03-11 22:13:44 +0800

    封装了购物车操作的所有api

--- src/apis/cart.js
+++ src/apis/cart.js
@@ -5,3 +5,46 @@ export const getCartAPI = () => {
     url: "/member/cart",
   });
 };
+
+export const addCartAPI = ({ skuId, count }) => {
+  return http({
+    url: "/member/cart",
+    method: "post",
+    data: {
+      skuId,
+      count,
+    },
+  });
+};
+
+export const changeCartAPI = ({ id, selected, count }) => {
+  return http({
+    url: `/member/cart/${id}`,
+    method: "PUT",
+    data: {
+      selected,
+      count,
+    },
+  });
+};
+
+export const deleteCartAPI = (ids) => {
+  return http({
+    url: "/member/cart",
+    method: "DELETE",
+    data: {
+      ids,
+    },
+  });
+};
+
+export const checkAllCartAPI = (selected, ids) => {
+  return http({
+    url: "/member/cart/selected",
+    method: "PUT",
+    data: {
+      selected,
+      ids,
+    },
+  });
+};

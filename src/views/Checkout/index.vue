85e72a32167e94670af6c811adaf60de1c1efe46
CarpTree <16690855026@163.com>
2026-03-12 17:19:01 +0800

    完成订单页面切换地址组件

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ElMessage } from "element-plus";
+import "element-plus/theme-chalk/el-message.css";
 import { getCheckoutInfoAPI } from "@/apis/checkout";
 import { onMounted, ref } from "vue";
 const checkInfo = ref({}); // 订单对象
@@ -9,6 +11,20 @@ const getCheckoutInfo = async () => {
   curAddress.value = checkInfo.value.userAddresses.find((item) => item.isDefault === 0);
 };
 onMounted(() => getCheckoutInfo());
+
+//切换地址弹窗
+const showDialog = ref(false);
+
+const activeAddress = ref({});
+const confirm = () => {
+  showDialog.value = false;
+  curAddress.value = activeAddress.value;
+};
+
+//添加地址没做
+const addAddress = () => {
+  ElMessage({ type: "warning", message: "暂未制作" });
+};
 </script>
 
 <template>
@@ -32,8 +48,8 @@ onMounted(() => getCheckoutInfo());
               </ul>
             </div>
             <div class="action">
-              <el-button size="large" @click="toggleFlag = true">切换地址</el-button>
-              <el-button size="large" @click="addFlag = true">添加地址</el-button>
+              <el-button size="large" @click="showDialog = true">切换地址</el-button>
+              <el-button size="large" @click="addAddress">添加地址</el-button>
             </div>
           </div>
         </div>
@@ -62,7 +78,7 @@ onMounted(() => getCheckoutInfo());
                   </a>
                 </td>
                 <td>&yen;{{ i.price }}</td>
-                <td>{{ i.price }}</td>
+                <td>{{ i.count }}</td>
                 <td>&yen;{{ i.totalPrice }}</td>
                 <td>&yen;{{ i.totalPayPrice }}</td>
               </tr>
@@ -113,6 +129,31 @@ onMounted(() => getCheckoutInfo());
     </div>
   </div>
   <!-- 切换地址 -->
+  <el-dialog title="切换收货地址" width="30%" center v-model="showDialog">
+    <div class="addressWrapper">
+      <div
+        class="text item"
+        :class="{ active: item.id === activeAddress.id }"
+        v-for="item in checkInfo.userAddresses"
+        @click="activeAddress = item"
+        :key="item.id"
+      >
+        <ul>
+          <li>
+            <span>收<i />货<i />人：</span>{{ item.receiver }}
+          </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="showDialog = false">取消</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
